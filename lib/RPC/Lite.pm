cc77d22a55c4ff6aa61cbe9d1bb2f73d6c41996b
Andy Burke <aburke@bitflood.org>
2005-08-12 01:14:43 +0000

    - placeholder/doc module

---
+++ lib/RPC/Lite.pm
@@ -0,0 +1,24 @@
+package RPC::Lite;
+
+# documentation/placeholder package
+
+our $VERSION = '0.0.1';
+
+=pod
+
+=head1 NAME
+
+  RPC::Lite - A lightweight yet flexible framework for remote process
+              communication.
+
+=head1 DESCRIPTION
+
+  Blah blah.
+
+=head1 EXAMPLES
+
+  ...
+
+=cut
+
+1;
