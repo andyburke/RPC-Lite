dc0dd4502e497b838bd004d4e7ea81514974d80b
Andy Burke <aburke@bitflood.org>
2006-07-27 05:50:47 +0000

    - add a couple of pure documentation packages

---
+++ lib/RPC/Lite/Transports.pm
@@ -0,0 +1,20 @@
+# DOCUMENTATION-ONLY PACKAGE
+package RPC::Lite::Transports;
+
+=pod
+
+=head1 NAME
+
+RPC::Lite::Transports -- A listing of supported transport layers.
+
+=head1 SUPPORTED TRANSPORT LAYERS
+
+=over 4
+
+=item TCP
+
+=back
+
+=cut
+
+1;
