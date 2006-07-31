2004317fc44d8be80f62ded8bf2436d1cadb33bd
Andy Burke <aburke@bitflood.org>
2006-07-31 20:25:35 +0000

    - document the Null serializer, including a warning that it is not currently supported

--- lib/RPC/Lite/Serializer/Null.pm
+++ lib/RPC/Lite/Serializer/Null.pm
@@ -1,4 +1,4 @@
-package RPC::Lite::Serializer::JSON;
+package RPC::Lite::Serializer::Null;
 
 use strict;
 use base qw( RPC::Lite::Serializer );
@@ -11,6 +11,28 @@ use RPC::Lite::Error;
 our $VERSION = '0.1';
 
 our $DEBUG = $ENV{DEBUG_SERIALIZER};
+
+=pod
+
+=head1 NAME
+
+RPC::Lite::Serializer::Null -- The 'null' serializer.
+
+=head1 SYNOPSIS
+
+B<WARNING>: The null serializer will not currently work
+with any of the suppoted transport layers.  The null serializer
+requires an "in-memory" transport layer between processes,
+which is not currently available.  This module is provided purely
+as reference at this point.
+
+=head1 DESCRIPTION
+
+RPC::Lite::Serializer::Null is meant for clients and servers
+operating on the I<same machine> that are both implemented in
+I<perl>.  It does not actually serialize the object.
+
+=cut
 
 sub VersionSupported
 {
